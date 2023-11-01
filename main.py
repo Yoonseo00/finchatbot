258ab2bb862a1d0ee27506ec987f182c52e87f29
Yoonseo00 <65036093+Yoonseo00@users.noreply.github.com>
2023-11-01 10:43:55 +0900

    Week9 capston

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/main')
-def main():
+def Main():
     return render_template('Main.html')
 
+@app.route('/alarm')
+def Alarm():
+    return render_template("Alarm.html")
+
+@app.route('/addspend')
+def AddSpend():
+    return render_template("AddSpend.html")
+
+@app.route('/spendlist')
+def SpendList():
+    return render_template("SpendList.html")
+
+@app.route('/')
+def test():
+    return render_template('AddR.html')
+
+@app.route('/result', methods = ['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        result = request.form
+        print(result)
+        return render_template("AddResult.html", result=result)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
