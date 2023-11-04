2d032089d1e7f7fdb136b2cf7a328a7a17919dac
oheh0311 <oheh0311@naver.com>
2023-11-04 13:04:23 +0900

    수정

--- graph1.py
+++ graph1.py
@@ -26,15 +26,42 @@ def load_age_category_data(age_group):
 
     return age_category_data
 
+# 현재 달 데이터 필터링 함수
+def filter_data_for_current_month(df):
+
+    # 현재 날짜 가져오기
+    current_date = datetime.now()
+
+    # 현재 달의 첫 날과 마지막 날 계산
+    current_month_first_day = datetime(current_date.year, current_date.month, 1)
+    current_month_last_day = datetime(current_date.year, current_date.month, current_date.day)
+
+    # 현재 달의 데이터 필터링
+    current_month_data = df[
+        (df['날짜'] >= current_month_first_day) &
+        (df['날짜'] <= current_month_last_day)
+    ]
+
+    return current_month_data
+
+# 현재 달의 총 소비액 계산 함수
+def calculate_current_month_total_expense(df):
+
+    current_month_data=filter_data_for_current_month(df)
+   
+    # 현재 달의 총 소비 금액 계산
+    current_month_total_expense = current_month_data['금액'].sum()
+
+    return current_month_total_expense
+
 # 현재 달의 카테고리별 소비 금액 계산 함수
-def current_consume_for_current_months(df):
+def category_consume_for_current_month(df):
 
-    current_month = datetime.now().month
-    filtered_data = df[df['날짜'].dt.month == current_month]
+    current_month_data=filter_data_for_current_month(df)
 
-    current_consume = filtered_data.groupby('카테고리')['금액'].sum().to_dict()
+    category_consume_current_month = current_month_data.groupby('카테고리')['금액'].sum().to_dict()
     
-    return current_consume
+    return category_consume_current_month
 
 
 # 최근 3개월치 데이터 필터링 함수
@@ -42,24 +69,41 @@ def filter_data_for_last_3_months(df):
     # 현재 날짜 계산
     end_date = datetime.now()
 
+    # 현재 월과 연도 가져오기
+    end_month = end_date.month
+    end_year = end_date.year
+
     # 3개월 전 날짜 계산
-    start_date = end_date - timedelta(days=90)
-    
+    if end_month < 3:
+    # 현재 월이 1월, 2월, 3월인 경우, 이전 연도의 10월, 11월, 12월 데이터를 포함
+        start_month = 9 + end_month
+        start_year = end_year - 1
+    else:
+    # 그 외의 경우, 현재 월로부터 3개월 전까지 데이터를 포함
+        start_month = end_month - 3
+        start_year = end_year
+
+    # 직전 달의 마지막 날짜 계산 (현재 월의 1일에서 1일을 빼면 직전 달의 마지막 날짜)
+    end_date = datetime(end_year, end_month, 1) - timedelta(days=1)
+
+    # 시작 날짜 계산
+    start_date = datetime(start_year, start_month, 1)
+
     # 최근 3개월치 데이터 필터링
     filtered_data = df[(df['날짜'] >= start_date) & (df['날짜'] <= end_date)]
-    
+
     return filtered_data
+
 
 # 최근 3개월 카테고리별 평균 소비 금액 계산 함수
 def category_avg_for_last_3_months(df):
 
-    filtered_data = filter_data_for_last_3_months(df)
-    category_avg = filtered_data.groupby('카테고리')['금액'].mean()
+    filtered_data=filter_data_for_last_3_months(df)
+    category_sum = filtered_data.groupby('카테고리')['금액'].sum()  # 각 카테고리별 금액의 합을 계산
+    category_avg = category_sum / 3  # 3으로 나누어 평균을 계산
+    category_avg = category_avg.to_dict()  # 결과를 딕셔너리로 변환
 
-    # 카테고리별 평균을 딕셔너리로 변환
-    category_avg_dict = category_avg.to_dict()
-    
-    return category_avg_dict
+    return category_avg
 
 
 # 3개월 평균 소비 그래프 생성 함수
@@ -94,36 +138,37 @@ def generate_graph(df):
 
 # 월별로 그룹화하여 총 소비 금액 계산 함수
 def calculate_monthly_total_expense(df):
+
     filtered_data = filter_data_for_last_3_months(df)
-    monthly_total_expense = filtered_data.groupby(filtered_data['날짜'].dt.strftime('%Y-%m'))['금액'].sum().to_dict()
-    
-    return monthly_total_expense
+
+    # 직전 3개월의 총 소비 금액 계산
+    previous_3_months_total_expense = filtered_data.groupby(filtered_data['날짜'].dt.strftime('%Y-%m'))['금액'].sum().to_dict()
+
+    return previous_3_months_total_expense
 
 #최근 3개월 카테고리별 평균 소비금액보다 많이 소비한 카테고리 계산 함수
 def find_exceeded_categories(df, category_avg):
 
     #현재 월 데이터 가져오기
-    current_month = datetime.now().month
-    current_month_data = df[df['날짜'].dt.month == current_month]
+    current_month_data = category_consume_for_current_month(df)
 
     # 초과한 카테고리와 그 차이를 저장하는 딕셔너리
     exceeded_categories = {}
 
-    for category in current_month_data['카테고리'].unique():
-        category_expense = current_month_data[current_month_data['카테고리'] == category]['금액'].sum()
-        if category_expense > category_avg.get(category, 0):
-            exceeded_amount = category_expense - category_avg[category]
+    for category in current_month_data:
+        if current_month_data[category] > category_avg.get(category, 0):
+            exceeded_amount = current_month_data[category] - category_avg[category]
             exceeded_categories[category] = exceeded_amount
 
     return exceeded_categories
 
 # 사용자가 선택한 연령대의 카테고리별 소비금액보다 많이 소비한 카테고리 계산 함수
 
-def find_categories_exceeding_age_group(df, age_group):
+def find_exceeded_age_group(df, age_group):
     age_category_data = load_age_category_data(age_group)
     age_category_avg = age_category_data.set_index('카테고리')['평균소비금액'].to_dict()
     
-    current_month_data = current_consume_for_current_months(df)
+    current_month_data = category_consume_for_current_month(df)
     
     exceeded_categories = {}
     for category in current_month_data:
@@ -185,22 +230,24 @@ def generate_comparison_graph(age_category_data, current_month_data):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     df = load_data('C:/finchatbot/exdata.csv')
-    current_month_data=current_consume_for_current_months(df)
     category_avg=category_avg_for_last_3_months(df)
     graph = generate_graph(df)
-    monthly_total_expense = calculate_monthly_total_expense(df)
+    current_month_total_expense=calculate_current_month_total_expense(df)
+    previous_3_months_total_expense = calculate_monthly_total_expense(df)
     exceeded_categories = find_exceeded_categories(df, category_avg)
+
+    age_group=None
     comparison_graph = []
     exceeded_categories_avg = None
 
     if request.method == 'POST':
         age_group = request.form.get('age_group')  # 사용자가 선택한 연령대 (웹 페이지에서 설정)
         age_category_data = load_age_category_data(age_group)
-        current_month_avg = current_consume_for_current_months(df)  # 이번 달 카테고리별 소비금액
-        comparison_graph = generate_comparison_graph(age_category_data, current_month_avg)
-        exceeded_categories_avg=find_categories_exceeding_age_group(df,age_group)
+        category_consume_current_month = category_consume_for_current_month(df)  # 이번 달 카테고리별 소비금액
+        comparison_graph = generate_comparison_graph(age_category_data, category_consume_current_month)
+        exceeded_categories_avg=find_exceeded_age_group(df,age_group)
 
-    return render_template('index.html', graph=graph, monthly_total_expense=monthly_total_expense, exceeded_categories=exceeded_categories, comparison_graph=comparison_graph,exceeded_categories_avg=exceeded_categories_avg, results=[])
+    return render_template('index.html', graph=graph, current_month_total_expense=current_month_total_expense, previous_3_months_total_expense=previous_3_months_total_expense, exceeded_categories=exceeded_categories, age_group=age_group, comparison_graph=comparison_graph,exceeded_categories_avg=exceeded_categories_avg, results=[])
 
 
 if __name__ == '__main__':
